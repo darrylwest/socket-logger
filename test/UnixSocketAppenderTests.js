7488572c0f569c570bb53d66a35d388262de6d3f
darryl west <darryl.west@RainCitySoftware.com>
2014-07-28 16:04:48 -0700

    added configuration and write tests for unix socket appender

--- test/UnixSocketAppenderTests.js
+++ test/UnixSocketAppenderTests.js
@@ -22,20 +22,64 @@ describe('UnixSocketAppender', function() {
         return new Logger( opts );
     };
 
+    var MockSocketClient = function() {
+        var mock = this,
+            messageCount = 0;
+
+        this.send = function(obj) {
+            messageCount++;
+            return obj;
+        };
+
+        this.getMessageCount = function() {
+            return messageCount;
+        };
+    };
+
     var createOptions = function() {
         var opts = {};
 
         opts.level = 'debug';
+        opts.socketClient = new MockSocketClient();
 
         return opts;
     };
 
     describe('#instance', function() {
-        var appender = new UnixSocketAppender( createOptions() );
+        var appender = new UnixSocketAppender( createOptions() ),
+            methods = [
+                'write',
+                'getTypeName',
+                'formatEntry',
+                'formatLevel',
+                'formatTimestamp'
+            ];
 
         it('should create an instance of UnixSocketAppender', function() {
             should.exist( appender );
             appender.should.be.instanceof( UnixSocketAppender );
+        });
+
+        it('should have all expected methods by size and type', function() {
+            dash.methods( appender ).length.should.equal( methods.length );
+            methods.forEach(function(method) {
+                appender[ method ].should.be.a( 'function' );
+            });
+        });
+    });
+
+    describe('write', function() {
+        var logger = createLogger();
+
+        it('should write a formatted json entry', function() {
+            var opts = createOptions(),
+                socketClient = opts.socketClient,
+                appender = new UnixSocketAppender( opts ),
+                entry = logger.createEntry('info', [ 'this is a test, time: ', new Date() ] );
+
+            socketClient.getMessageCount().should.equal( 0 );
+            appender.write( entry );
+            socketClient.getMessageCount().should.equal( 1 );
         });
     });
 });
